983fc1588e689ed326ec6c086f584c7d89135cd9
Hemitha123 <hemithatech@gmail.com>
2026-02-09 20:30:19 +0530

    Add examples for tuples, dictionaries, and sets

--- prog3.py
+++ prog3.py
@@ -8,6 +8,7 @@
 #it cannot be appended or change the existing element because tuple is immutable
 
 #Convert a list into a tuple and a tuple into a list.
+
 llist=[1,4,2,"hemitha",40.23,"amar"]
 tup=tuple(llist)
 print(tup)
@@ -17,11 +18,13 @@ list1=list(tup1)
 print(list1)
 
 # Find the index of a given element in a tuple.
+
 ttup=(10,20,55,"hemitha","ananya",30)
 res=ttup.index("hemitha")
 print(res)
 
 # Create a dictionary with names as keys and ages as values. Print names of people older than 18.
+
 ddict={"amar":22,"hemitha":20,"himani":15,"moksha":12}
 for name,age in ddict.items():
     if age>18:
@@ -29,6 +32,7 @@ for name,age in ddict.items():
 
 
 #Add, update, and delete keys in a dictionary.
+
 ddict={"amar":101,"vaishnavi":102,"moksha":103,"hemitha":104}
 print(ddict)
 ddict["syed"]=105
@@ -41,6 +45,7 @@ ddict.pop("amar")
 print(ddict)
 
 #Count the frequency of each character in a given string using a dictionary.
+
 ddict={}
 s=input("enter the string")
 for ch in s:
@@ -51,6 +56,7 @@ for ch in s:
 print(ddict)
 
 #Merge two dictionaries into one
+
 ddict={"amar":101,"monisha":102}
 ddict2={"hemitha":103,"moksha":104}
 ddict3={}
@@ -59,6 +65,7 @@ ddict3.update(ddict2)
 print(ddict3)
 
 #Given a dictionary of students and marks, find who scored the highest
+
 ddict={'amar': 99, 'monisha': 100, 'hemitha': 67, 'moksha': 90}
 high=0
 hname=""
@@ -69,6 +76,7 @@ for name,marks in ddict.items():
 print(f"the highest scored is {hname} with {high}")        
 
 #Create two sets of numbers and perform union, intersection, and difference.
+
 sset1=set()
 n=int(input("enter the number"))
 for x in range(n):
@@ -89,11 +97,13 @@ print(interaction_set)
 print(difference_set)
 
 #Remove duplicates from a list using a set.
+
 llist=[10,2,20,50,48,30,10,3,2,10]
 llist1=list(set(llist))
 print(llist1)
 
 #Check if one set is a subset of another.
+
 set1={2,4,2,2,2}
 set2={2,4,2,2,2}
 print(set1.issubset(set2))
